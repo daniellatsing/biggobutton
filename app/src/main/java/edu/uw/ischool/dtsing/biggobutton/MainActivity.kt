af466ad4930d8a3f3f211aca357ee621a355727b
Daniella <daniellatsing@gmail.com>
2024-01-28 21:53:14 -0800

    moved folder content and completed button visual

---
+++ app/src/main/java/edu/uw/ischool/dtsing/biggobutton/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.uw.ischool.dtsing.biggobutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
